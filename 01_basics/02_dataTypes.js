a31f9c0e85cf6da8ea494ce74bfec2666bd650ac
Vedansh Gupta <vedanshgupta06@gmail.com>
2025-08-30 09:06:03 +0000

    datatypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"Use strict";//treat all JS code as a newer version
+
+// alert("Hello")   we are using nodejs not browser it works in browser
+
+
+let name="Vedansh"
+let age=18
+let isLoggedIn=false
+let state=null
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true false
+//null =>standalone value
+//undefined => value not assigned
+//symbol=> unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
